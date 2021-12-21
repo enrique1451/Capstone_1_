a83b885f03a2173fa821e34b3bea54b64c1625df
Enrique Hoyos <enrique145@yahoo.com>
2021-12-21 17:53:50 -0500

    minor improvements

--- forms.py
+++ forms.py
@@ -1,35 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField, IntegerField, FieldList
-from wtforms.validators import DataRequired, Length, Optional, Email
-from wtforms.ext.sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
+from wtforms.validators import InputRequired, Length, Optional, Email, EqualTo
 from models import Diet
 
 
 class UserAddForm(FlaskForm):
     """Form for adding users."""
 
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[Length(min=6)])
-    username = StringField('Username', validators=[DataRequired()])
-
-# class SelectDietsForm(FlaskForm):
-#     """Form for selecting diets for new or existing logged in user"""
-#     diets = QuerySelectMultipleField(query_factory = lambda:Diet.query.all())
+    email = StringField('E-mail', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired(), EqualTo("confirm", message="Passwords don't match"), Length(min=6, message="Password Requirements Not Met")])
+    confirm = PasswordField("Retype Password")
+    username = StringField('Username', validators=[InputRequired()])
 
 
 
 class LoginForm(FlaskForm):
     """Login form."""
 
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[Length(min=6)])
+    email = StringField('E-mail', validators=[InputRequired(), Email(message="Email not entered")])
+    password = PasswordField('Password', validators=[InputRequired("Entry Required"), Length(min=6)])
 
 
 class RecipeForm(FlaskForm):
     """Form for entering recipe to be analyzed"""
-    title = StringField('Title', validators=[DataRequired()])
-    servings = IntegerField('Servings', validators=[DataRequired()]) 
-    ingredients = TextAreaField('Ingredients (Enter valid quantities (lbs, tsp, etc) and use a " , " after entering each ingredient.', validators=[DataRequired()])
+    title = StringField('Title', validators=[InputRequired("Recipe Title Required")])
+    servings = IntegerField('Servings', validators=[InputRequired("Servings required")]) 
+    ingredients = TextAreaField('Ingredients (Enter valid quantities (lbs, tsp, etc) and use a "," after entering each ingredient.', validators=[InputRequired()])
     instructions = TextAreaField('Instructions (This field can be left empty if no cooking instructions are available)', validators=[Optional(strip_whitespace=True)])
 
 
